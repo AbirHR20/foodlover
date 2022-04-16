9be6796efae160fd360340e5459014da10c377cc
unknown <you@example.com>
2022-04-17 03:59:56 +0600

    first commit

---
+++ js/custom.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+    //navbar
+
+    var nav = $('nav');
+
+    $(window).on('scroll', function(){
+        var scrolling = $(this).scrollTop();
+
+        if(scrolling>100){
+            nav.addClass('navfix');
+        }
+        else{
+            nav.removeClass('navfix');
+        }
+    });
+
+    //wow js
+
+    new WOW().init();
+
+    //type js
+
+    var typed = new Typed('.element', {
+        strings: ["Breakfast", "Launch", "Dinner"],
+        typeSpeed: 70,
+        loop : true,
+      });
+
+});
